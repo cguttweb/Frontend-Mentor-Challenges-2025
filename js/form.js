67cefbdafc176fac5c014c8b2bfb6e693d193824
Chloe <cguttweb@pm.me>
2025-08-25 19:43:07 +0100

    ty message styling improving validation

--- js/form.js
+++ js/form.js
@@ -5,15 +5,32 @@ const message = document.getElementById('message')
 const consent = document.getElementById('consent')
 const errors = document.querySelectorAll('.error')
 const inputs = document.querySelectorAll('input[type="text"], input[type="email"], textarea')
+const submitButton = document.querySelector('.submitBtn')
 
 function showErrors() {
   errors.forEach(error => error.style.display = 'block');
   inputs.forEach(input => input.style.border = '1px solid red');
 }
 
+function showThankYouMessage() {
+  document.querySelector('.ty-msg').style.display = 'block'
+}
+
 function validateForm() {
-  if (firstName.value == '' || lastName.value == '' || email.value == '' || message.value == '' || consent.checked == false) {
+
+  if (firstName.value == '' || lastName.value == '' || email.value == '' || message.value == '') {
     showErrors()
     return false
   }
+  else if (consent.checked === false) {
+    document.querySelector('.consent-error').style.display = 'block'
+  }
+
+  // showThankYouMessage()
+
 }
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  validateForm();
+});
